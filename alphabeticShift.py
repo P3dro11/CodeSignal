da790bb7c82821b504a67f70c1c9fcba9bac3ddf
Pedro B <palexb101@gmail.com>
2022-10-01 20:19:08 -0700

    30 cs

--- alphabeticShift.py
+++ alphabeticShift.py
@@ -3,3 +3,14 @@ Given a string, your task is to replace each of its characters by the next one i
 replace a with b, replace b with c, etc (z would be replaced by a).
 
 """
+
+def solution(inputString):
+    
+    answer = ""
+    
+    for i in inputString:
+        if i == "z":
+            answer += "a"
+        else:
+            answer += chr(ord(i)+1)
+    return answer
